ef27485a32d380e43ce25e97e42b8fe4d9f24886
0xDepp <a@xsq.pw>
2019-10-22 22:21:17 +0100

    Error

--- index.php
+++ index.php
@@ -3,6 +3,10 @@
 require 'vendor/autoload.php';
 require 'src/php/functions.inc.php'
 
+error_reporting(E_ALL);
+ini_set('display_errors', TRUE);
+ini_set('display_startup_errors', TRUE);
+
 $klein = new \Klein\Klein();
 
 $klein->respond('GET', '/', function () {
